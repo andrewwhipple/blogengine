65c47a165a046aa9cf11fe7379e3c20a851609ce
Andrew Whipple <awhipp28@stanford.edu>
2016-03-28 13:09:34 -0700

    Let's see if this works

---
+++ installations.sh
@@ -0,0 +1,20 @@
+clear
+
+sudo apt-get install git
+
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y build-essential
+
+git clone https://github.com/andrewwhipple/blogengine.git
+
+cd blogengine
+
+sudo npm install
+
+cd ~
+
+wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
---
+++ run-dropbox.sh
@@ -0,0 +1,5 @@
+clear
+
+echo "Starting up dropbox"
+~/.dropbox-dist/dropboxd &
+echo "Done!"
