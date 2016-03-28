c10f4de690b7196a3156f68b36d785b787b4a09e
Andrew Whipple <awhipp28@stanford.edu>
2016-03-28 12:32:01 -0700

    Changed the ports

--- engine.js
+++ engine.js
@@ -202,7 +202,7 @@ var processPost = function(postData) {
 }
 
 //The server!
-app.listen(3000, function() {
-    console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port ' + process.env.PORT);
 });
 
