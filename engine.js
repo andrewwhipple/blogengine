2cc18cc84955bb448df3325b020899a73437ef2c
Andrew Whipple <android128@gmail.com>
2017-07-03 17:40:42 -0500

    Hmmm

--- engine.js
+++ engine.js
@@ -151,6 +151,7 @@ function getBlogroll(res, numPosts, searchString) {
 
 		for (var i = 0; i < numPosts; i++) {
 			if (i < postList.posts.length && postList.posts[i].toString().indexOf(searchString) !== -1) {
+				console.log(postList[i]);
 				blogRollPosts[i] = globalVars.appConfig.filePath + '/blog/' + postList.posts[i];
 			} 
 		}
